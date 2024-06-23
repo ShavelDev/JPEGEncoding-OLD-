51188330e5ef4b19e924f634f71cb74c384fd80d
ShavelDev <nor.szaw@gmail.com>
2024-06-23 22:06:41 +0200

    Initial Commit

---
+++ JPEGSingleBlockEncoding/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  JPEGSingleBlockEncoding
+//
+//  Created by Norbert Szawloga on 23/06/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
